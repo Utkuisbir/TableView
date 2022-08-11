7a0893d97b841f2ba9161bb2cbe07f1bf1887973
utku isbir <utkuisbir@icloud.com>
2022-08-11 08:34:26 +0300

    Initial Commit

---
+++ TableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by muammer utku isbir on 11.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
